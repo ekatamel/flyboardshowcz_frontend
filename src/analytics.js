b763d6ea28ba03687806ee24854e9c67d5b7d882
Ekaterina Melnichuk <katerinka.melnichuk@gmail.com>
2025-03-31 23:02:49 +0700

    GA setup

--- src/analytics.js
+++ src/analytics.js
@@ -1,12 +1,9 @@
 import ReactGA from 'react-ga4'
 
-// Initialize Google Analytics
 export const initGA = () => {
-  // TODO set in env variables
-  ReactGA.initialize('G-1PYFQB299S')
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_CLIENT)
 }
 
-// Track Page View
 export const trackPageView = path => {
   ReactGA.send({ hitType: 'pageview', page: path })
 }
