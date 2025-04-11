3db863c5433b886db6eec195bcf5457817873a44
Ekaterina Melnichuk <katerinka.melnichuk@gmail.com>
2025-04-11 21:15:25 +0700

    chore(sentry): install and set up Sentry for issues monitoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
+import * as Sentry from '@sentry/react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_URL,
+})
 
 const rootElement = document.getElementById('root')!
 const root = ReactDOM.createRoot(rootElement)
